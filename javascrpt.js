25254ea06e448a573be1617a5928a09a51ca4f6a
Chetan Sharma <chetan70001@gmail.com>
2022-03-04 20:19:02 +0530

    dashboard

---
+++ javascrpt.js
@@ -0,0 +1,14 @@
+const accordion = document.getElementsByClassName('container');
+
+for (i=0; i<accordion.length; i++) {
+  accordion[i].addEventListener('click', function () {
+    this.classList.toggle('active')
+  })
+}
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
